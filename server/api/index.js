327f6079f41048ec04e2619be479130d586cee5d
Tsolmongerel Bayasaa - G <25LP0666@administrators-iMac.local>
2026-04-03 10:43:04 +0800

    api fixed

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("API ажиллаж байна 🚀");
+  try {
+    res.status(200).send("API ажиллаж байна 🚀");
+  } catch (error) {
+    res.status(500).json({
+      message: "Route crashed",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
